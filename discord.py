645f25fb78b837cc5a24875f87cee7c4f6d47413
takemoto-eye-sales <t.mizuki@x-opg.com>
2023-02-09 21:55:57 +0900

    feat: new_competitionの機能を追加

--- discord.py
+++ discord.py
@@ -7,6 +7,7 @@ import sys
 import json
 from discordwebhook import Discord
 from kaggle import KaggleApi
+from datetime import datetime, timedelta
 
 # コマンドエラー時に表示するコマンド例を作成
 command_json = json.load(open('command.json', 'r'))
@@ -28,8 +29,32 @@ if command == 'competitions_list':
   # discordの設定
   DISCORD_WEBHOOK_URL_COMPETITION_LIST = os.environ['DISCORD_WEBHOOK_URL_COMPETITION_LIST']
   discord = Discord(url=DISCORD_WEBHOOK_URL_COMPETITION_LIST)
-  competition_list = api.competitions_list()
-  print(competition_list)
-  # discord.post(content="Hello, world.")
+  competitions_list = api.competitions_list()
+  discord.post(content='開催中のコンペ')
+  for competition in competitions_list:
+    # 常設コンペは除外
+    if competition.hasCategory and competition.category == 'Getting Started':
+     continue
+    send_message = f'\nタイトル：{competition.title if competition.hasTitle else "なし"}'
+    send_message += f'\nURL：{competition.ref}'
+    send_message += f'\n賞金：{competition.reward if competition.hasReward else "なし"}'
+    send_message += f'\n〆切：{competition.deadline}'
+    discord.post(content=send_message)
+elif command == 'new_competition':
+  # discordの設定
+  DISCORD_WEBHOOK_URL_COMPETITION_LIST = os.environ['DISCORD_WEBHOOK_URL_COMPETITION_LIST']
+  discord = Discord(url=DISCORD_WEBHOOK_URL_COMPETITION_LIST)
+  competitions_list = api.competitions_list()
+  previous_date = datetime.now() - timedelta(days=1)
+  current_date = datetime.now()
+  for competition in competitions_list:
+    # 常設コンペは除外
+    if previous_date < competition.enabledDate and competition.enabledDate <= current_date:
+     continue
+    send_message = f'\nタイトル：{competition.title if competition.hasTitle else "なし"}'
+    send_message += f'\nURL：{competition.ref}'
+    send_message += f'\n賞金：{competition.reward if competition.hasReward else "なし"}'
+    send_message += f'\n〆切：{competition.deadline}'
+    discord.post(content=send_message)
 else:
   raise Exception(f'そのようなコマンドは存在しません。\n{command_example}')
